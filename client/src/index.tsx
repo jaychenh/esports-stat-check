49fbf6d6abd914a48a2e01e23ef5885755170c38
Jay Chenh <83093869+jaychenh@users.noreply.github.com>
2024-11-25 16:13:33 -0500

    Setting up React + Vite + TS

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <h1>Hello, TypeScript and React!</h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
